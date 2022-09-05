758e85f10f7aee77b1ef0588a6ab2bce77eee58f
S-Ventura <bohemian_rapsody@hotmail.es>
2022-09-05 00:10:18 +0000

    Cambios viernes

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,7 +1,6 @@
 //@ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
     // 'sap/ui/model/json/JSONModel'
     // 'dtt/SAPUI5/model/Models',
     // 'sap/ui/model/resource/ResourceModel'
@@ -10,15 +9,9 @@ sap.ui.define([
      * @param{typeof sap.ui.core.mvc.Controller} Controller
      * @param{typeof sap.m.MessageToast} MessageToast
      */
-    function (Controller, MessageToast) {
-        return Controller.extend("dtt.SAPUI5.controller.App", {
+    function (Controller) {
+        return Controller.extend("dtt.SAPUI5.controller.App ", {
             onInit: function () {
             },
-            onShowHello: function () {
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMSG = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMSG);
-            }
         });
     });
